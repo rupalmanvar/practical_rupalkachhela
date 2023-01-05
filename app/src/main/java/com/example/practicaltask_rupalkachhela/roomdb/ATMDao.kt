f53cdd67fd545b042edab8fe8eab353ef3279045
rupalkachhela96 <rupal.kachhela@excellentwebworld.in>
2023-01-05 11:17:55 +0530

    change logs : => transactions update

--- app/src/main/java/com/example/practicaltask_rupalkachhela/roomdb/ATMDao.kt
+++ app/src/main/java/com/example/practicaltask_rupalkachhela/roomdb/ATMDao.kt
@@ -6,6 +6,7 @@ import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.example.practicaltask_rupalkachhela.model.AddAmount
 
+
 @Dao
 interface ATMDao : WithdrawAmountDao,NotesDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
